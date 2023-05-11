d6612a5f7bdbc3dd93f64171b48760e31ac97023
KimSeungTae <iufan1526@gmail.com>
2023-05-11 22:02:16 +0900

    aysnc await 사용하여 변경

--- lolSerachApp/RequestAPI.swift
+++ lolSerachApp/RequestAPI.swift
@@ -13,7 +13,7 @@ class RequestAPI {
     (DataModel) -> Void) {
         
         let baseUrl = "https://kr.api.riotgames.com"
-        let apiKey = "RGAPI-d9e2890e-4856-4b92-bac8-ae25b55dafb8"
+        let apiKey = "RGAPI-f35fdc26-eb95-454d-85d2-01948b96b88b"
         let serachUrl = "/lol/summoner/v4/summoners/by-name/"
         
         let urlSession = URLSession.shared
@@ -44,55 +44,9 @@ class RequestAPI {
             do {
                 let decoder = JSONDecoder()
                 var resultDataModel = try decoder.decode(DataModel.self, from: data)
-                
-                self.requestMatchInfo(id: (resultDataModel.id ?? "")) { data in
-                    resultDataModel.rank = data.rank
-                    completion(resultDataModel)
-                }
-            }catch {
-                print("데이터 파싱 실패!")
-                return
-            }
-        }
-        task.resume()
-    }
-    
-    static func requestMatchInfo(id: String, completion: @escaping (UserInfoDataModel) -> Void) {
-        
-        let baseUrl = "https://kr.api.riotgames.com"
-        let apiKey = "RGAPI-d9e2890e-4856-4b92-bac8-ae25b55dafb8"
-        let searchUrl = "/lol/league/v4/entries/by-summoner/"
-        
-        let urlSession = URLSession.shared
-        
-        guard let id = id.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)else {
-            return
-        }
-        
-        guard let url = URL(string: baseUrl + searchUrl + id + "?api_key=" + apiKey)else {
-            return
-        }
-        print(baseUrl + searchUrl + id + "?api_key=" + apiKey)
-        let request = URLRequest(url: url)
-        
-        let task = urlSession.dataTask(with: request) { data, response, error in
-            
-            if let error = error {
-                print(error)
-                return
-            }
-            
-            guard let data = data else {
-                print("데이터 가져오기 실패!")
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let resultDataModel = try decoder.decode(UserInfoDataModel.self, from: data)
                 completion(resultDataModel)
             }catch {
-                print("데이터 파싱 실패! 2")
+                print("데이터 파싱 실패!")
                 return
             }
         }
--- lolSerachApp/SearchViewController.swift
+++ lolSerachApp/SearchViewController.swift
@@ -17,20 +17,69 @@ class SearchViewController: UIViewController {
     }
 
     @IBAction func SearchMoveToDetail(_ sender: Any) {
-        guard let summonerName = self.userName.text else{
-            return
+
+        Task {
+            do {
+                guard let summonerName = self.userName.text else{
+                    return
+                }
+                
+                let resultData = try await getData(summonerName: summonerName)
+                
+                DispatchQueue.main.async {
+                    let detailVC = DetailViewController(nibName: "DetailViewController", bundle: nil)
+                    detailVC.userName = (resultData.name ?? "")
+                    
+                    self.navigationController?.pushViewController(detailVC, animated: true)
+                }
+                
+            }catch {
+                
+            }
+            
         }
         
-        RequestAPI.requestSummonerInfo(summonerName: summonerName) { data in
-            DispatchQueue.main.async {
-                let detailVC = DetailViewController(nibName: "DetailViewController", bundle: nil)
-                detailVC.userName = (data.name ?? "")
-                print(data.rank ?? "x")
-                
-                self.navigationController?.pushViewController(detailVC, animated: true)
-            }
-        }
+//        RequestAPI.requestSummonerInfo(summonerName: summonerName) { data in
+//            DispatchQueue.main.async {
+//                let detailVC = DetailViewController(nibName: "DetailViewController", bundle: nil)
+//                detailVC.userName = (data.name ?? "")
+//
+//                self.navigationController?.pushViewController(detailVC, animated: true)
+//            }
+//        }
     }
     
+    func getData(summonerName: String) async throws -> DataModel {
+        
+        let baseUrl = "https://kr.api.riotgames.com"
+        let apiKey = "RGAPI-f35fdc26-eb95-454d-85d2-01948b96b88b"
+        let serachUrl = "/lol/summoner/v4/summoners/by-name/"
+        
+        guard let encodingName = summonerName.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {
+            throw MyErorr.errorIncoding
+        }
+        
+        guard let url = URL(string:baseUrl + serachUrl + encodingName + "?api_key=" + apiKey) else {
+            throw MyErorr.invalidUrl
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw MyErorr.badResponse
+        }
+        
+        let userInfo = try JSONDecoder().decode(DataModel.self, from: data)
+        
+        return userInfo
+    }
+    
+}
+
+enum MyErorr: Error {
+    
+    case errorIncoding
+    case invalidUrl
+    case badResponse
 }
 
